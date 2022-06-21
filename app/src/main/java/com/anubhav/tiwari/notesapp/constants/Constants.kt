c3b64438d0b9724cd3b0f2237e328d4c384bc69b
Anubhav Tiwari <mail2anubhavt@gmail.com>
2022-06-21 15:19:15 +0530

    Dependency Injection - Dagger Hilt.

--- app/src/main/java/com/anubhav/tiwari/notesapp/constants/Constants.kt
+++ app/src/main/java/com/anubhav/tiwari/notesapp/constants/Constants.kt
@@ -2,4 +2,10 @@ package com.anubhav.tiwari.notesapp.constants
 
 object Constants {
     val IGNORE_AUTH_URL = listOf("/login","/register")
+
+    const val DATABASE_NAME = "notes_db"
+
+    const val BASE_URL = "http://10.0.2.2:8080"
+
+    const val ENCRYPTED_SHARED_PREF_NAME = "enc_shared_pref"
 }
--- app/src/main/java/com/anubhav/tiwari/notesapp/data/local/NotesDatabase.kt
+++ app/src/main/java/com/anubhav/tiwari/notesapp/data/local/NotesDatabase.kt
@@ -1,11 +1,12 @@
 package com.anubhav.tiwari.notesapp.data.local
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.anubhav.tiwari.notesapp.data.local.entities.Note
 
 @Database(entities = [Note::class], version = 1)
 @TypeConverters(Converters::class)
-abstract class NotesDatabase {
+abstract class NotesDatabase:RoomDatabase() {
     abstract fun noteDao(): NoteDao
 }
