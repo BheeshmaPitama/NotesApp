5554f70e989d8197d25cde91c0204dd4eed13d75
Anubhav Tiwari <mail2anubhavt@gmail.com>
2022-06-25 16:49:16 +0530

    Setting up Caching Mechanism.

--- app/src/main/java/com/anubhav/tiwari/notesapp/repositories/NoteRepository.kt
+++ app/src/main/java/com/anubhav/tiwari/notesapp/repositories/NoteRepository.kt
@@ -2,10 +2,14 @@ package com.anubhav.tiwari.notesapp.repositories
 
 import android.app.Application
 import com.anubhav.tiwari.notesapp.data.local.NoteDao
+import com.anubhav.tiwari.notesapp.data.local.entities.Note
 import com.anubhav.tiwari.notesapp.data.remote.NoteApi
 import com.anubhav.tiwari.notesapp.data.remote.requests.AccountRequest
 import com.anubhav.tiwari.notesapp.utils.Resource
+import com.anubhav.tiwari.notesapp.utils.checkForInternet
+import com.anubhav.tiwari.notesapp.utils.networkBoundResource
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 import java.lang.Exception
 import javax.inject.Inject
@@ -15,6 +19,19 @@ class NoteRepository @Inject constructor(
     private val noteApi: NoteApi,
     private val context: Application,
 ) {
+
+    fun getAllNotes(): Flow<Resource<List<Note>>>{
+        return networkBoundResource(
+            query = {noteDao.getAllNotes()},
+            fetch = {noteApi.getNotes()},
+            saveFetchResult = { response ->
+                response.body()?.let {
+                    //TODO: Insert Notes In Database
+                }
+            },
+            shouldFetch = { checkForInternet(context)}
+        )
+    }
 
     suspend fun login(email:String,password:String)= withContext(Dispatchers.IO){
         try {
